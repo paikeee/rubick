861db2256fcce290a1c277c847e9ce7964ffea35
paikeee <serzhik2002@gmail.com>
2022-05-22 23:35:23 +0300

    ver. 1.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,6 @@ import javafx.fxml.*;
 
 public class Main extends Application {
 
-
     @Override
     public void start(Stage stage) throws Exception {
 
@@ -18,9 +17,7 @@ public class Main extends Application {
 
     }
 
-
     public static void main(String[] args) {
         launch(args);
     }
-
 }
